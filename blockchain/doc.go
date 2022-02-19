3d40fe3a019bf4190994976a21ef489728076aa5
KonradStaniec <konrad.staniec@gmail.com>
2022-02-19 11:42:32 +0100

    Remove bip0030

--- blockchain/doc.go
+++ blockchain/doc.go
@@ -75,7 +75,6 @@ Bitcoin Improvement Proposals
 This package includes spec changes outlined by the following BIPs:
 
 		BIP0016 (https://en.bitcoin.it/wiki/BIP_0016)
-		BIP0030 (https://en.bitcoin.it/wiki/BIP_0030)
 		BIP0034 (https://en.bitcoin.it/wiki/BIP_0034)
 */
 package blockchain
